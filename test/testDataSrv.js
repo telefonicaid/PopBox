f1dda56e0dc039f6ddd29e67034016b68ce502cc
Marcos Reyes <mru@tid.es>
2012-03-01 12:50:15 +0100

    try to avoid a confilc in testmodule

--- test/testDataSrv.js
+++ test/testDataSrv.js
@@ -1,8 +1,7 @@
 var dataSrv = require('../src/DataSrv.js');
-var redis_module = require('../src/node_modules/redis');
+var redis_module = require('redis');
 var config = require('../src/config.js');
 var rc = redis_module.createClient(redis_module.DEFAULT_PORT, config.redis_server); //will be a list of servers
-
 var provision = {
     "payload":"MESSAGE",
     "priority":"H",
@@ -14,8 +13,7 @@ var provision = {
     "expirationDate": Math.round((new Date()).getTime() / 1000)+10000,
     "expirationDelay": 260
 };
-
-/*dataSrv.push_transaction(provision, function (err, transaction_id) {
+dataSrv.push_transaction(provision, function (err, transaction_id) {
     'use strict';
     console.dir("ERR: " + err);
     console.dir("TID: " + transaction_id);
@@ -27,9 +25,7 @@ var provision = {
     }
     //check REDIS
 
-}); */
-
-
+});
 dataSrv.pop_notification({'id':'4QT9JX'},2,function (err, transaction_id) {
     'use strict';
     console.dir("ERR: " + err);
