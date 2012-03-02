f11dfa1953b81065e90a624841cd2d8106a85fe3
Carlos Romero Brox <brox@tid.es>
2012-03-02 11:02:29 +0100

    first server for devices

--- src/ConsumerSrv.js
+++ src/ConsumerSrv.js
@@ -1,2 +1,31 @@
-//Accept BoxPop (short and Long) requests
-var DataSrv = require('./DataSrv');
+var http = require('http');
+var connect = require('connect');
+var url = require('url');
+
+var dataSrv = require('./DataSrv');
+
+
+var app = connect();
+
+app.use('/', function (req, res) {
+
+        var path = url.parse(req.url);
+        var queue_id = path.pathname.slice(1);
+        console.log(queue_id);
+
+        dataSrv.pop_notification(queue_id, 1000, function (err, notif_list) {
+            if (err) {
+                res.writeHead(500, {'content-type':'text/plain'});
+                res.write(String(err));
+                res.end();
+            }
+            else {
+                res.writeHead(200, {'content-type':'application/json'});
+                res.write(JSON.stringify(notif_list));
+                res.end();
+            }
+        });
+    }
+);
+
+app.listen(3003);
