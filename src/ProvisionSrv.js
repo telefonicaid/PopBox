9b13ce022202771e08e79a54256280cde4d8e815
Carlos Romero Brox <brox@tid.es>
2012-03-07 09:00:23 +0100

    add 'blocking' path

--- src/ProvisionSrv.js
+++ src/ProvisionSrv.js
@@ -3,6 +3,7 @@ var connect = require('connect');
 
 var config = require('./config.js').provision;
 var dataSrv = require('./DataSrv');
+var dataSrvBl = require('./DataSrvBlocking');
 var validate = require('./validate');
 
 var app = connect();
@@ -13,32 +14,41 @@ app.use(connect.query());
 
 app.use(connect.bodyParser());
 
+
+app.use('/block', function (req, res) {
+    insert(req, res, dataSrvBl.blocking_push, validate.errors_trans);
+});
+
+
 app.use('/', function (req, res) {
-
-        console.log(req.body);
-
-        var errors = validate.errors_trans(req.body);
-
-        if(errors.length == 0){
-            dataSrv.push_transaction(req.body, function (err, trans_id) {
-                if (err) {
-                    res.writeHead(500, {'content-type':'application/json'});
-                    res.write(JSON.stringify({error: err}));
-                    res.end();
-                }
-                else {
-                    res.writeHead(200, {'content-type':'application/json'});
-                    res.write(JSON.stringify({id:trans_id}));
-                    res.end();
-                }
-            });
-        }
-        else {
-            res.writeHead(400, {'content-type':'application/json'});
-            res.write(JSON.stringify({error: errors}));
-            res.end();
-        }
-    }
-);
+    insert(req, res, dataSrv.push_transaction, validate.errors_trans);
+});
 
 app.listen(config.port);
+
+function insert(req, res, push, validate) {
+
+    console.log(req.body);
+
+    var errors = validate(req.body);
+
+    if (errors.length == 0) {
+        push(req.body, function (err, trans_id) {
+            if (err) {
+                res.writeHead(500, {'content-type':'application/json'});
+                res.write(JSON.stringify({error:err}));
+                res.end();
+            }
+            else {
+                res.writeHead(200, {'content-type':'application/json'});
+                res.write(JSON.stringify({id:trans_id}));
+                res.end();
+            }
+        });
+    }
+    else {
+        res.writeHead(400, {'content-type':'application/json'});
+        res.write(JSON.stringify({error:errors}));
+        res.end();
+    }
+}
