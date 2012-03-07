e714cc3443da74a325feaf0ab6a444b33e5d5280
Carlos Romero Brox <brox@tid.es>
2012-03-07 11:09:09 +0100

    add blocking pop

--- src/ConsumerSrv.js
+++ src/ConsumerSrv.js
@@ -1,12 +1,37 @@
 var http = require('http');
 var connect = require('connect');
 var url = require('url');
+var util = require('util');
 
 var config = require('./config.js').consumer;
 var dataSrv = require('./DataSrv');
+var dataSrvBl = require('./DataSrvBlocking');
 
 
 var app = connect();
+
+app.use('/block', function (req, res) {
+
+        var path = url.parse(req.url);
+        console.log(path);
+        var queue_id = path.pathname.slice(1);
+        console.log(queue_id);
+
+        dataSrvBl.blocking_pop(queue_id, /*blocking time, config*/ 2, function (err, notif) {
+            if (err) {
+                res.writeHead(500, {'content-type':'text/plain'});
+                res.write(String(err));
+                res.end();
+            }
+            else {
+                message = notif && notif[1]?notif[1]:null;
+                res.writeHead(200, {'content-type':'application/json'});
+                res.write(JSON.stringify(message));
+                res.end();
+            }
+        });
+    }
+);
 
 app.use('/', function (req, res) {
 
