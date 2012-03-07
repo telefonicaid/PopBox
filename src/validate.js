b6681ea928fa2cde44e50a63c379ee17ee215e32
Carlos Romero Brox <brox@tid.es>
2012-03-07 07:55:26 +0100

    polishing error messages

--- src/validate.js
+++ src/validate.js
@@ -9,12 +9,12 @@ function errors_trans(trans) {
         errors.push("undefined queue");
     }
     else if(trans.queue.constructor != Array) {
-        errors.push("invalid queue (should be an Array)");
+        errors.push("invalid queue type");
     }
     else {
         trans.queue.forEach(function( value, index){
             if(!value || !value.id) {
-                errors.push("invalid queue element " + index);
+                errors.push("invalid queue element");
             }
         });
     }
