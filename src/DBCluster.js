771fee49b0c701502a85a3be1b0d75a3be6afeb6
Marcos Reyes <mru@tid.es>
2012-03-07 16:17:00 +0100

    added redis.select for multiDB (test/production) support

--- src/DBCluster.js
+++ src/DBCluster.js
@@ -4,7 +4,7 @@ var redis_module = require('redis');
 var config = require('./config.js');
 
     var rc = redis_module.createClient(redis_module.DEFAULT_PORT, config.redis_server); //will be a list of servers
-
+    rc.select(config.selected_db);
     var get_db = function(queu_id){
         'use strict';
         //returns a client from a cluster
