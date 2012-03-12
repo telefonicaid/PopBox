ec1f61cf88c857aca4c2feb9ca472800a0b0f320
Marcos Reyes <mru@tid.es>
2012-03-12 13:00:42 +0100

    improve dataSrv test

--- test/testDataSrv.js
+++ test/testDataSrv.js
@@ -1,86 +1,110 @@
 var dataSrv = require('../src/DataSrv.js');
-var redis_module = require('../src/node_modules/redis');
 var config = require('../src/config.js');
-var rc = redis_module.createClient(redis_module.DEFAULT_PORT, config.redis_server); //will be a list of servers
+var a = require('assert');
+var async = require('../src/node_modules/async');
+
 var provisionH = {
     "payload":"MESSAGEH",
     "priority":"H",
     "callback":"http://foo.bar",
     "queue":[
-        {"id":"4QT9J"},
-        {"id":"4QT9K"},
-        {"id":"4QT9L"}
-
+        {"id":"TST4QT9J"},
+        {"id":"TST4QT9K"}
     ],
-    "expirationDate": Math.round((new Date()).getTime() / 1000)+10000,
-    "expirationDelay": 260
+    "expirationDate":Math.round((new Date()).getTime() / 1000) + 10000,
+    "expirationDelay":260
 };
 var provisionL = {
     "payload":"MESSAGEL",
     "priority":"L",
     "callback":"http://foo.bar",
     "queue":[
-        {"id":"4QT9J"}
+        {"id":"TST4QT9J"}
     ],
-    "expirationDate": Math.round((new Date()).getTime() / 1000)+10000,
-    "expirationDelay": 260
+    "expirationDate":Math.round((new Date()).getTime() / 1000) + 10000,
+    "expirationDelay":260
 };
-exports.push = function(){
-dataSrv.push_transaction(provisionH, function (err, transaction_id) {
+exports.push = function () {
     'use strict';
-    console.dir("ERRH: " + err);
-    console.dir("TIDH: " + transaction_id);
-    if (provisionH.expirationDate){
-    console.log("TIME TIMESTAMP: "+ provisionH.expirationDate);
-    }
-    else{
-        console.log("TIME DELAY SECONDS: "+ provisionH.expirationDelay);
-    }
-    //check REDIS
+    dataSrv.push_transaction(provisionH, function (err, transaction_id) {
+        a.ifError(err);
+        a.ok(transaction_id, "Generated T_ID_H: ");
 
-});
+    });
 
-dataSrv.push_transaction(provisionL, function (err, transaction_id) {
-    'use strict';
-    console.dir("ERRL: " + err);
-    console.dir("TIDL: " + transaction_id);
-    if (provisionL.expirationDate){
-        console.log("TIME TIMESTAMP: "+ provisionL.expirationDate);
-    }
-    else{
-        console.log("TIME DELAY SECONDS: "+ provisionL.expirationDelay);
-    }
-    //check REDIS
+    dataSrv.push_transaction(provisionL, function (err, transaction_id) {
+        a.ifError(err);
+        a.ok(transaction_id, "Generated T_ID_L: ");
 
-});
+    });
 };
 
-exports.pop = function(max){
-    dataSrv.pop_notification({'id':'4QT9J'},max,function (err, transaction_id) {
+exports.pop = function (id, max, callback) {
     'use strict';
-    if (err){
-    console.log('ERROR:');
-    console.dir(err);
-    }
-        else{
-        console.log('DATA:');
-        console.dir(transaction_id);
-    }
-});
+    return function (callback) {
+        dataSrv.pop_notification({'id':id}, max, function (err, transaction_id) {
+            void (callback && callback(err, transaction_id));
+
+        });
+    };
+};
+var get_data = function (ext_transaction_id, state, summary) {
+    'use strict';
+    dataSrv.get_transaction(ext_transaction_id, state, summary, function (err, data) {
+        a.ifError(err);
+        a.ok(data, "get_data");
+    });
 };
 
-var get_data = function(ext_transaction_id, state, summary){
+exports.test_push_pop = function () {
     'use strict';
-    dataSrv.get_transaction(ext_transaction_id, state, summary, function(err, data){
-         if(err){
-             console.log("from test: "+err);
-         }
-        else{
-             console.log('DATA_TEST:');
-             console.dir(data);
-         }
-     });
+    console.log("TESTING CARDINALITY -multi queue/multi priority-");
+
+    //flush the queues
+    console.log("FLUSHING THE QUEUES");
+    async.series([exports.pop('TST4QT9J', 100), exports.pop('TST4QT9K', 100)], function (err) {
+        a.ifError(err, 'ERROR FLUSHING QUEUES');
+
+        //push some data
+        console.log("PUSHING DATA  AT TST4QT9J (2xH / 2xL) and TST4QT9K (2xH)");
+
+        exports.push(); //pushes messages in three queues(2xH/1xL)
+        exports.push(); //pushes messages in three queuesx2(2xH/1xL)
+        setTimeout(function () {
+            console.log("GET 3 ELEMS FROM TST4QT9J (3 expected)");
+            exports.pop('TST4QT9J', 3, function (err, data) {
+                a.ifError(err, "error at test1");
+                a.equal(data.length, 3, " TEST 1 : EXPECTED THREE ELEM LIST get " + data.length);
+            }); //gets two H and one L
+        }, 1000);
+        setTimeout(function () {
+            console.log("GET 3 ELEMS FROM TST4QT9J (1 expected)");
+            exports.pop('TST4QT9J', 3, function (err, data) {
+                a.ifError(err, "error at test2");
+                a.equal(data.length, 1, " TEST 2 : EXPECTED ONE ELEM LIST get " + data.length);
+            }); //gets one from L
+
+        }, 1500);
+        setTimeout(function () {
+            console.log("GET 3 ELEMS FROM TST4QT9J (0 expected)");
+            exports.pop('TST4QT9J', 3, function (err, data) {
+                a.ifError(err, "error at test3");
+                a.equal(data.length, 0, " TEST 3 : EXPECTED EMPTY ELEM LIST get " + data.length);
+            }); //gets empty
+        }, 2000);
+        setTimeout(function () {
+            console.log("GET 100 ELEMS FROM TST4QT9K (2 expected)");
+            exports.pop('TST4QT9K', 100, function (err, data) {
+                a.ifError(err, "error at test4");
+                a.equal(data.length, 2, " TEST 4 : EXPECTED TWO ELEM LIST get " + data.length);
+            }); //gets empty
+            console.log("TEST ENDS OK");
+        }, 2500);
+    });
+
 };
 
 exports.get_data = get_data;
+
+exports.test_push_pop();
 
