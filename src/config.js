b51f19146d8aced554c98b3c1e44a8fb1ff6ab8b
Marcos Reyes <mru@tid.es>
2012-03-28 11:49:45 +0200

    change prefix for keys

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 exports.redis_server = 'Relay1';
 exports.selected_db = 0; //0..15 for   0 ->pre-production 1->test
-exports.db_key_queue_prefix = 'PB:Q:';
-exports.db_key_trans_prefix = 'PB:T:';
-exports.db_key_blocking_queue_prefix = 'PB:B:';
+exports.db_key_queue_prefix = 'PB:Q|';
+exports.db_key_trans_prefix = 'PB:T|';
+exports.db_key_blocking_queue_prefix = 'PB:B|';
 
 exports.consumer = {};
 exports.consumer.max_messages = 1000;
