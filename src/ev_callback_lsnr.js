89f4239f4cd555c0feab0f77e9a354c87f01c87f
Marcos Reyes <mru@tid.es>
2012-04-09 09:59:58 +0200

    Async ready callback listener

--- src/ev_callback_lsnr.js
+++ src/ev_callback_lsnr.js
@@ -1,7 +1,30 @@
-/**
- * Created with JetBrains WebStorm.
- * User: mru
- * Date: 09/04/12
- * Time: 08:52
- * To change this template use File | Settings | File Templates.
- */
+//FIRE AND FORGET
+var http = require('http');
+var url = require('url');
+
+var init = function (emitter) {
+    "use strict";
+    return function(callback){
+    emitter.on("NEWSTATE", function (data) {
+        //Just act on delivered
+        if (data.state === "Delivered") {
+            do_callback(data);
+        }
+    });
+    callback && callback(null);
+};
+};
+
+function do_callback(data) {
+    "use strict";
+    if (data.callback) {
+        var options = url.parse(data.callback);
+        var cb_req = http.request(options);  //FIRE AND FORGET
+        var str_data = JSON.stringify(data);
+        cb_req.on('error', function foo(err) {
+            console.log('callback err::' + err);
+        });
+        cb_req.write(str_data);
+        cb_req.end();
+    }
+}
