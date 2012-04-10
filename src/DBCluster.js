e39c96fd8e66557cf1543ba2dcde7af82099a292
Marcos Reyes <mru@tid.es>
2012-04-10 14:28:48 +0200

    JSHINT and GJSLINT passed

--- src/DBCluster.js
+++ src/DBCluster.js
@@ -8,32 +8,52 @@
 var redis_module = require('redis');
 var config = require('./config.js');
 
-var rc = redis_module.createClient(redis_module.DEFAULT_PORT, config.redis_server); //will be a list of servers
+var rc = redis_module.createClient(redis_module.DEFAULT_PORT,
+                                   config.redis_server);
 rc.select(config.selected_db);
-var getDb = function (queu_id) {
-    'use strict';
-    var rc = redis_module.createClient(redis_module.DEFAULT_PORT, config.redis_server); //will be a list of servers
-    rc.select(config.selected_db);
-    //returns a client from a cluster
-    return rc;
+
+var getDb = function(queu_id) {
+  'use strict';
+  var rc = redis_module.createClient(redis_module.DEFAULT_PORT,
+                                     config.redis_server);
+  rc.select(config.selected_db);
+  //returns a client from a cluster
+  return rc;
 };
 
-var getTransactionDb = function (transaction_id) {
-    'use strict';
-    if (!rc || !rc.connected) {
-        rc = redis_module.createClient(redis_module.DEFAULT_PORT, config.redis_server);
-    }
-    //return a client for transactions
-    return rc;
+var getTransactionDb = function(transaction_id) {
+  'use strict';
+  if (!rc || !rc.connected) {
+    rc =
+      redis_module.createClient(redis_module.DEFAULT_PORT, config.redis_server);
+  }
+  //return a client for transactions
+  return rc;
 
 };
 
-var free = function (db) {
-    //return to the pool TechDebt
-    "use strict";
-    db.end();
+var free = function(db) {
+  //return to the pool TechDebt
+  'use strict';
+  db.end();
 };
 
+/**
+ *
+ * @param {string} queu_id identifier.
+ * @return {Object} rc redis client for QUEUES.
+ */
 exports.get_db = getDb;
+
+/**
+ *
+ * @param {string} transaction_id valid uuid identifier.
+ * @return {Object}  rc redis Client for Transactions.
+ */
 exports.get_transaction_db = getTransactionDb;
+
+/**
+ *
+ * @param {Object} db Redis DB to be closed.
+ */
 exports.free = free;
