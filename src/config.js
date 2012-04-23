d23ac43fdabcae4f4ca8220d56316e32eb56776e
brox <brox@tid.ers>
2012-04-23 09:18:53 +0200

    removed "consumer" & "provision" configuration

--- src/config.js
+++ src/config.js
@@ -36,36 +36,6 @@ exports.db_key_blocking_queue_prefix = 'PB:B|';
  *
  * @type {Object}
  */
-exports.consumer = {};
-/**
- *
- * @type {Number}
- */
-exports.consumer.max_messages = 1000;
-/**
- *
- * @type {Number}
- */
-exports.consumer.port = 3003;
-/**
- *
- * @type {Number}
- */
-exports.consumer.pop_timeout = 30;
-/**
- *
- * @type {Object}
- */
-exports.provision = {};
-/**
- *
- * @type {Number}
- */
-exports.provision.port = 3000;
-/**
- *
- * @type {Object}
- */
 exports.ev_lsnr = {};
 /**
  *
