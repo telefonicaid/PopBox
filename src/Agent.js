a38ab4ebb9f13a77e3cdaf5b1a1c2225d67a6bea
Carlos Romero Brox <brox@tid.es>
2012-06-06 12:49:01 +0200

    new logging system + new operations

--- src/Agent.js
+++ src/Agent.js
@@ -2,12 +2,21 @@
 // Copyright (c) Telefonica I+D. All rights reserved.
 //
 //
+
+
 var config = require('./config.js');
 var cluster = require('cluster');
 var numCPUs = require('os').cpus().length;
 
+var path = require('path');
+var log = require('PDITCLogger');
+var logger = log.newLogger();
+logger.prefix = path.basename(module.filename,'.js');
+
+
 if (config.cluster.numcpus >= 0 && config.cluster.numcpus < numCPUs) {
     numCPUs = config.cluster.numcpus;
+    logger.debug('numCPUs='+numCPUs);
 }
 
 
@@ -19,9 +28,11 @@ if (cluster.isMaster && numCPUs !== 0) {
 
     cluster.on('death', function (worker) {
         'use strict';
-        console.log('worker ' + worker.pid + ' died');
+        logger.warning('worker ' + worker.pid + ' died');
     });
 } else {
+    logger.debug('cluster worker',cluster);
+
     var fs = require('fs');
     var express = require('express');
     var logic = require('./agent_logic');
@@ -62,8 +73,12 @@ if (cluster.isMaster && numCPUs !== 0) {
     appSec.get('/queue/:id', function(req, res) {logic.checkPerm(appSec.prefix, req, res, logic.getQueue);});
     appSec.post('/queue', function(req, res) {logic.postQueue(appSec.prefix, req, res);});
 
-    app.post('/trans', function(req, res) {logic.postTrans(app.prefix,req,res);});
+
+    app.delete('/trans/:id_trans', logic.deleteTrans);
     app.get('/trans/:id_trans/:state?', logic.transState);
+    app.post('/trans/:id_trans/payload', logic.payloadTrans);
+    app.post('/trans/:id_trans/expireAt', logic.expireTrans);
+    app.post('/trans', function(req, res) {logic.postTrans(app.prefix,req,res);});
     app.get('/queue/:id/size', function(req, res) { logic.queueSize(app.prefix,req,res)});
     app.get('/queue/:id', function(req, res) {logic.getQueue(app.prefix, req, res );});
 
@@ -73,13 +88,15 @@ if (cluster.isMaster && numCPUs !== 0) {
     async.parallel([evLsnr.init(emitter), cbLsnr.init(emitter)],
         function onSubscribed() {
             'use strict';
+           // logger.debug('onSubscribed()', []);
             servers.forEach(function(server){server.listen(server.port);});
         });
+    servers.forEach(function(server){server.listen(server.port);});
 }
 
 process.on('uncaughtException', function (err) {
     'use strict';
-    console.log('PROCESS %s', err);
+    logger.warning(err);
 });
 
 
