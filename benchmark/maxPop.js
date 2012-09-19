ac2a7898c127cf5895908905fe3fc5c80b7407a8
Fernando Godino <fgodino@conwet.com>
2012-09-19 14:49:11 +0200

    maxPop updated

--- benchmark/maxPop.js
+++ benchmark/maxPop.js
@@ -14,42 +14,47 @@ var async = require('async');
 
 var provision = genProvision.genProvision(1, config.payload_length);
 
-async.series([
 
+async.series([
     function (callback) {
         var cont = 0;
-
-
-        for (var i = 0; i < config.numPops; i++) {
-            dataSrv.pushTransaction('UNSEC:', provision, function (err, res) {
-                console.log(res);
-                cont++;
-                console.log(cont);
-                if (cont == config.numPops) callback();
-            });
-        }
+        var int = setInterval(function () {
+            for (var i = 0; i < config.numPops; i++) {
+                dataSrv.pushTransaction('UNSEC:', provision, function (err, res) {
+                    console.log(res);
+                    cont++;
+                    console.log(cont);
+                    if (cont == config.numPops){
+                        clearInterval(int);
+                        callback();
+                    }
+                });
+            }
+        }, 3000);
     },
     function (callback) {
         var cont = 0;
         console.time('Total time');
-        setInterval(function(){},3000);
-        for (var i = 0; i < config.numPops; i++) {
+        for (var i = 0; i < 1000; i++) {
             rest.post(config.protocol + '://' + config.hostname
                 + ':' + config.port + '/queue/' + provision.queue[0].id + '/pop?max=1',
                 { headers: {'Accept': 'application/json'}}).on('complete', function (data, response) {
-                    //console.log(data);
                     cont++;
-                    console.log(cont);
-                    if (!response) callback('error: ' + data);
-                    else if (data.data === '[]') callback(error);
-                    if (cont == config.numPops) callback();
+                    if (response === null) {
+                        callback('Error', null);
+                    }
+                    else if (data.data === '[]') {
+                        callback('Error', null);
+                    }
+                    else if (cont == config.numPops) {
+                        callback(null, 'success');
+                    }
                 });
         }
 
     }
-],
-    function () {
-        console.timeEnd('Total time');
-    }
-)
-;
+], function (err, results) {
+    if (err) console.log(err);
+    if (results) console.log(results[1]);
+    console.timeEnd('Total time');
+});
