dfe66abdd948b0c49899c088f9c29a784d788c00
Fernando Godino <fgodino@conwet.com>
2012-10-02 12:39:28 +0200

    update randomTest

--- benchmark/randomTest.js
+++ benchmark/randomTest.js
@@ -1,118 +1,123 @@
-/**
- * Created with JetBrains WebStorm.
- * User: fernando
- * Date: 24/09/12
- * Time: 11:38
- * To change this template use File | Settings | File Templates.
- */
+var config = require('./config.js');
+var http = require('http');
+var rest = require('restler');
+http.globalAgent.maxSockets = 20000;
+
 
 function randomize(min, max) {
     'use strict';
     return Math.round(Math.random() * (max - min)) + min;
 }
 
-var doForever = function () {
+var genProvision = function (numQueues, payloadSize) {
     'use strict';
-    var pushOrPop = randomize(0, 9);
 
-    if (pushOrPop < 5) {
-        var queues_Push = randomize(1, 10);
-        var payload_length = randomize(500, 5000);
-        var times_push = randomize(1, 50);
-        console.log("hace push con " + queues_Push + ' colas y ' + payload_length + ' bytes (' + times_push + 'veces)');
-        provision(queues_Push, payload_length, times_push, function (data, response) {
-            console.log("ha llegado");
-        });
-    }
-    else {
-        var times_Pop = randomize(1, 1000);
-        var queue_id = 'q' + randomize(0, 10);
-        doPop(times_Pop, queue_id);
-        console.log("hace pop a " + queue_id + ' ' + times_Pop + ' veces');
+    //Variables
+    var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+    var queuesArray = [];
+    var provisionContent = '';
+
+    //Content
+    for (var i = 0; i < payloadSize; i++) {
+        var charPos = Math.floor(Math.random() * CHARS.length);
+        provisionContent += CHARS.charAt(charPos);
     }
 
-    var nextTime = randomize(1000, 60000);
-    console.log("siguiente en " + nextTime + ' ms');
+    //Queues
+    var j = Math.round(Math.random() * numQueues);
 
-    setTimeout(doForever, nextTime);
+    for (var i = 0; i < numQueues; i++) {
+        queuesArray[i] = {};
+        queuesArray[i].id = 'q' + j++;
 
-
-};
-
-/**
- * Created with JetBrains WebStorm.
- * User: oelmaallem
- * Date: 24/09/12
- * Time: 11:37
- * To change this template use File | Settings | File Templates.
- */
-
-var rest = require('restler');
-var config = require('./config.js');
-
-
-var genProvision = function (num_pops, payload_size) {
-
-    var queues_array = [];
-    var string_length = payload_size;
-
-    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-    var randomstring = '';
-
-    for (var i = 0; i < string_length; i++) {
-        var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum, rnum + 1);
+        if (j > config.randomTest.maxQueues) {
+            j = 0;
+        }
     }
 
-    var j = Math.random() * num_pops;
-    /* if (Math.round(j)>=num_pops){
-     j=num_pops-1;
-     }*/
-    for (var i = 0; i < num_pops; i++) {
-        /* if (Math.round(j)>=num_pops){
-         break;
-         }*/
-        queues_array[i] = {};
-        queues_array[i].id = 'q' + Math.round(j);
-        j++;
-    }
-
+    //Provision
     var provision = {};
 
-    provision.payload = randomstring;
+    provision.payload = provisionContent;
     provision.priority = 'H';
-    provision.queue = queues_array;
+    provision.queue = queuesArray;
 
     return provision;
 };
 
-var provision = function (numQueues, payload_length, N, callback) {
+var provision = function (numQueues, payloadLength, timesPush, callback) {
     'use strict';
-    var provision = genProvision(numQueues, payload_length);
-    for (var i = 0; i < N; i++) {
-        rest.postJson(config.protocol + '://' + config.hostname + ':' + config.port + '/trans',
-            provision).on('complete', function (data, response) {
-                console.log(data);
-                callback(data, response);
-            });
+
+    //Variables
+    var provision = genProvision(numQueues, payloadLength);
+    var hostname = config.agentsHosts[0].host;
+    var port = config.agentsHosts[0].port;
+
+    for (var i = 0; i < timesPush; i++) {
+
+        rest.postJson(config.protocol + '://' + hostname + ':' + port + '/trans',
+            provision)/*.on('complete', function (data, response) {
+         provisionsCompleted++;
+         })*/;
+
     }
 };
 
-var http = require('http');
+var doPop = function (nPops, queue){
+    'use strict';
 
-http.globalAgent.maxSockets = 20000;
-
-var doPop = function (n_times, queue){
+    //Variables
     var interval = Math.round(Math.random() * 10);
-    var timeout = Math.round(Math.random() * (300 - 1) + 1);
-    console.log('timeout : ' + timeout);
-    var max = Math.round(Math.random() * (1000 - 1) + 1);
-    for(var i = 0; i < n_times; i++){
+    var timeOut = randomize(config.randomTest.minTimeOut, config.randomTest.maxTimeOut);
+    var maxMessages = randomize(1, config.randomTest.maxMessages);
+    var hostname = config.agentsHosts[0].host;
+    var port = config.agentsHosts[0].port;
+
+    for(var i = 0; i < nPops; i++){
         setTimeout(function(){
-            rest.post(config.protocol + '://' + config.hostname + ':'
-                + config.port + '/queue/' + queue + '/pop?timeout=' + timeout + '&max=' + max).on('complete', function(data, response){
-                    console.log(data);
-                });
+            rest.post(config.protocol + '://' + hostname + ':' + port + '/queue/' + queue +
+                '/pop?timeout=' + timeOut + '&max=' + maxMessages)/*.on('complete', function(data, response){
+             //console.log('Pop received: ' + data);
+             })*/;
         }, interval);
     }
 };
+
+var doForever = function () {
+    'use strict';
+
+    var pushOrPop = randomize(0, 9);
+
+    if (pushOrPop < 5) {
+
+        //Variables
+        var numQueues = randomize(1, config.randomTest.maxQueues);
+        var payloadLength = randomize(config.randomTest.minPayloadLength, config.randomTest.minPayloadLength);
+        var timesPush = randomize(1, config.randomTest.maxTimesPush);
+
+        console.log('Pushing in ' + numQueues + ' queues with provisions of ' + payloadLength + ' bytes ' + timesPush + ' times');
+        provision(numQueues, payloadLength, timesPush);
+
+    } else {
+
+        //Variables
+        var timesPop = randomize(1, config.randomTest.maxTimesPop);
+        var queueID = 'q' + randomize(0, config.randomTest.maxQueues);
+
+        console.log('Popping queue ' + queueID + ' ' + timesPop + ' times');
+        doPop(timesPop, queueID);
+
+    }
+
+    var nextTime = randomize(1000, 60000);
+    console.log('Next action in ' + nextTime/1000 + ' s');
+
+    setTimeout(doForever, nextTime);
+};
+
+doForever();
+
+
+
+
+
