53002b344f225acd7eaa23554dd40aa14baa77d0
Fernando Godino <fgodino@conwet.com>
2012-10-24 10:16:05 +0200

    more reliable tests

--- tests/test/inboxTest.js
+++ tests/test/inboxTest.js
@@ -9,9 +9,9 @@ var protocol = config.protocol;
 
 var trans, trans1 = {};
 
-describe('Inbox', function() {
+describe('Inbox', function () {
 
-    afterEach(function(done) {
+    afterEach(function (done) {
         this.timeout(8000); //Mocha timeout
         var urlQ1 = protocol + '://localhost:' + port +
             '/queue/q1/Pop';
@@ -19,121 +19,109 @@ describe('Inbox', function() {
             '/queue/q2/Pop';
         var completed = 0;
 
-        rest.post(urlQ1).on('complete', function() {
+        rest.post(urlQ1).on('complete', function () {
             completed++;
             if (completed == 2) done();
         });
-        rest.post(urlQ1).on('complete', function() {
+        rest.post(urlQ2).on('complete', function () {
             completed++;
             if (completed == 2) done();
         });
     });
 
 
-    it('Should return all the transactions', function(done) {
-
-        var trans;
-        var listaTrans = [];
-
-        var trans1 = {
-            'payload': 'Prueba 1',
+    it('Should return all the transactions', function (done) {
+        this.timeout(8000); //Mocha timeout
+        var trans = {
+            'payload': 'Test',
             'priority': 'H',
             'callback': protocol + '://foo.bar',
             'queue': [
                 { 'id': 'q1' },
                 { 'id': 'q2' }
-            ],
-            'expirationDate': Math.round(new Date().getTime() / 1000 + 60)
-        };
-
-        var trans2 = {
-            'payload': 'Prueba 2',
-            'priority': 'H',
-            'callback': protocol + '://foo.bar',
-            'queue': [
-                { 'id': 'q1' },
-                { 'id': 'q2' }
-            ],
-            'expirationDate': Math.round(new Date().getTime() / 1000 + 60)
+            ]
         };
 
         async.series([
-            function(callback) {
+            function (callback) {
                 rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-                    trans1).on('complete', function(data, response) {
-                        trans = {id: data.data, value: trans1};
-                        listaTrans.push(trans);
+                    trans).on('complete', function (data, response) {
                         callback();
                     });
             },
-            function(callback) {
-
-
-                rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-                    trans2).on('complete', function(data, response) {
-                        trans = {id: data.data, value: trans2};
-                        listaTrans.push(trans);
-                        callback();
-
-                    });
-            },
-            function(callback) {
+            function (callback) {
 
                 rest.post(protocol + '://' + host + ':' + port + '/queue/q1/pop')
-                    .on('complete', function(data, response) {
-                        data.data.length.should.be.equal(2);
-                        data.data.should.include('Prueba 1');
-                        data.data.should.include('Prueba 2');
+                    .on('complete', function (data, response) {
+                        data.data.length.should.be.equal(1);
+                        data.data.should.include('Test');
                         callback();
                     });
-            }], function() {
+            },
+            function (callback) {
+
+                rest.post(protocol + '://' + host + ':' + port + '/queue/q2/pop')
+                    .on('complete', function (data, response) {
+                        data.should.have.property('data');
+                        data.data.length.should.be.equal(1);
+                        data.data.should.include('Test');
+                        callback();
+                    });
+            }
+        ], function () {
             done();
         });
     });
 
-    it('Should return the high priority transaction', function(done) {
+    it('Should return the high priority transaction', function (done) {
 
-        var trans_nueva;
-
-        var trans3 = {
-            'payload': 'Baja prioridad',
+        var transLow = {
+            'payload': 'Low priority',
             'priority': 'L',
             'callback': protocol + '://foo.bar',
             'queue': [
                 { 'id': 'q1' },
                 { 'id': 'q2' }
-            ],
-            'expirationDate': Math.round(new Date().getTime() / 1000 + 60)
+            ]
         };
-        rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-            trans3).on('complete', function(data, response) {
-            });
 
-        var trans4 = {
-            'payload': 'Alta prioridad',
+        var transHigh = {
+            'payload': 'High priority',
             'priority': 'H',
             'callback': protocol + '://foo.bar',
             'queue': [
-                { 'id': 'q1' },
-                { 'id': 'q2' }
-            ],
-            'expirationDate': Math.round(new Date().getTime() / 1000 + 60)
+                { 'id': 'q1' }
+            ]
         };
 
-        rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-            trans4).on('complete', function(data, response) {
-                trans_nueva = {id: data.data, value: trans4};
-            });
-
-        rest.post(protocol + '://' + host + ':' + port + '/queue/q1/pop?max=1')
-            .on('complete', function(data, response) {
-                data.data.length.should.be.equal(1);
-                'Alta prioridad'.should.be.equal(data.data.pop());
-                done();
-            });
+        async.series([
+            function (callback) {
+                rest.postJson(protocol + '://' + host + ':' + port + '/trans',
+                    transLow).on('complete', function (data, response) {
+                        callback();
+                    });
+            },
+            function (callback) {
+                rest.postJson(protocol + '://' + host + ':' + port + '/trans',
+                    transHigh).on('complete', function (data, response) {
+                        callback();
+                    });
+            },
+            function (callback) {
+                rest.post(protocol + '://' + host + ':' + port + '/queue/q1/pop?max=1')
+                    .on('complete', function (data, response) {
+                        data.should.have.property('data');
+                        data.data.length.should.be.equal(1);
+                        (data.data.pop()).should.be.equal('High priority');
+                        callback();
+                    });
+            }
+        ], function () {
+            done();
+        });
     });
 
-    it('Should return empty data (timeout)', function(done) {
+    it('Should return empty data (timeout)', function (done) {
         this.timeout(8000); //Mocha timeout
 
         var trans5 = {
@@ -141,28 +129,27 @@ describe('Inbox', function() {
             'priority': 'L',
             'callback': protocol + '://foo.bar',
             'queue': [
-                { 'id': 'q1' },
-                { 'id': 'q2' }
+                { 'id': 'q1' }
             ],
             'expirationDate': Math.round(new Date().getTime() / 1000 + 60)
         };
 
 
         var funcs = [
-            function(cb) {
+            function (cb) {
                 rest.post(protocol + '://' + host + ':' + port + '/queue/q1/pop?timeout=1',
                     {headers: {'Accept': 'application/json'}})
-                    .on('complete', function(data, response) {
+                    .on('complete', function (data, response) {
                         //console.log(data);
                         data.data.length.should.be.equal(0);
                         cb();
 
                     });
             },
-            function(cb) {
-                setTimeout(function() {
+            function (cb) {
+                setTimeout(function () {
                     rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-                        trans5).on('complete', function(data, response) {
+                        trans5).on('complete', function (data, response) {
                             cb();
                         });
                 }, 6000);
@@ -170,41 +157,40 @@ describe('Inbox', function() {
             }
         ];
 
-        var cb = function() {
+        var cb = function () {
             done();
         };
 
         async.parallel(funcs, cb);
     });
 
-    it('Should not return empty data (timeout)', function(done) {
+    it('Should not return empty data (timeout)', function (done) {
         this.timeout(8000); //Mocha timeout
         var trans6 = {
             'payload': 'Prueba timeout',
             'priority': 'L',
             'callback': protocol + '://foo.bar',
             'queue': [
-                { 'id': 'q1' },
-                { 'id': 'q2' }
+                { 'id': 'q1' }
             ],
             'expirationDate': Math.round(new Date().getTime() / 1000 + 60)
         };
 
 
         var funcs = [
-            function(cb) {
+            function (cb) {
                 rest.post(protocol + '://' + host + ':' + port + '/queue/q1/pop?timeout=7',
                     {headers: {'Accept': 'application/json'}})
-                    .on('complete', function(data, response) {
+                    .on('complete', function (data, response) {
                         data.data.length.should.be.equal(1);
                         data.data.should.include('Prueba timeout');
                         cb();
                     });
             },
-            function(cb) {
-                setTimeout(function() {
+            function (cb) {
+                setTimeout(function () {
                     rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-                        trans6).on('complete', function(data, response) {
+                        trans6).on('complete', function (data, response) {
                             cb();
                         });
                 }, 3000);
@@ -212,7 +198,7 @@ describe('Inbox', function() {
             }
         ];
 
-        var cb = function() {
+        var cb = function () {
             done();
         };
 
