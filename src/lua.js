02727a17bbabef3acb53738c3fd773f6cad6eb1c
Marcos Reyes <mru@tid.es>
2012-10-25 09:34:58 +0200

    Add the right header TEF(c) AGPL

---
+++ src/lua.js
@@ -0,0 +1,59 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mru
+ * Date: 24/10/12
+ * Time: 09:12
+ * To change this template use File | Settings | File Templates.
+ */
+
+var redisModule = require('redis');
+var rc = redisModule.createClient(6379, 'localhost');
+var keys = ['uno', 'dos','tres'];
+var argv = [10];
+
+var joined = keys.concat(argv);
+
+var popScript="\
+local rollback = function(data, key)\n\
+  for i, elem in pairs(data) do\n\
+    redis.call('lpush',key, elem)\n\
+  end\n\
+end\n\
+local rollbackAll = function(rollbackData)\n\
+  for k,d in pairs(rollbackData) do\n\
+    rollback(d, k)\n\
+  end\n\
+end\n\
+local max = 0+ARGV[1]\n\
+local err = nil\n\
+local resultIndex=1\n\
+local result={}\n\
+local totalLength=0\n\
+local rollbackData = {}\n\
+for j, k in pairs(KEYS) do\n\
+  if max > totalLength then\n\
+    local data = redis.pcall('lrange', k, 0, max-1-totalLength)\n\
+    local dataLength = table.getn(data)\n\
+    local trimerr = redis.pcall('ltrim', k, dataLength, -1)\n\
+    if ((not data.err) and (not trimerr.err)) then\n\
+      rollbackData[k] = data\n\
+    end\n\
+    if (data.err or trimerr.err) then\n\
+      rollbackAll(rollbackData)\n\
+      if trimerr.err then return {err = trimerr.err} end\n\
+      if data.err then return {err = data.err} end\n\
+    end\n\
+    result[resultIndex]=data\n\
+    resultIndex = resultIndex + 1\n\
+    totalLength = totalLength + dataLength\n\
+   end\n\
+end\n\
+return result\n\
+";
+
+
+rc.eval(popScript, keys.length, joined, function(err, data){
+  "use strict";
+  console.dir(err);
+  console.dir(data);
+});
--- src/sendrender.js
+++ src/sendrender.js
@@ -1,9 +1,15 @@
-/**
- * Created with JetBrains WebStorm.
- * User: brox
- * Date: 21/06/12
- * Time: 12:57
- * To change this template use File | Settings | File Templates.
+/*
+ Copyright 2012 Telefonica Investigación y Desarrollo, S.A.U
+
+ This file is part of PopBox.
+
+ PopBox is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ PopBox is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License along with PopBox
+ . If not, seehttp://www.gnu.org/licenses/.
+
+ For those usages not covered by the GNU Affero General Public License please contact with::dtc_support@tid.es
  */
 
 function sendRender() {
