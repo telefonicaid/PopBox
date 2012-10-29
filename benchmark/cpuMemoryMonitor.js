7d650b9f8409d20cfba8de45d2a1bbd8ca682fdd
Aitor Magán <amagan@conwet.com>
2012-10-29 10:14:40 +0100

    Revert "cpuMemoryMonitor.js renamed to utils.js and added a new feature (getChildProcesses)"
    This reverts commit 20ad2747aae25d1a68662a5802c8d66938e6c597.

---
+++ benchmark/cpuMemoryMonitor.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+
+var getUserUsage = function (pid, cb) {
+    fs.readFile('/proc/' + pid + '/stat', function (err, data) {
+        var elems = data.toString().split(' ');
+        var utime = parseInt(elems[13]);
+        var stime = parseInt(elems[14]);
+
+        cb(utime + stime);
+    });
+}
+
+var getSysUsage = function (cb) {
+    fs.readFile('/proc/stat', function (err, data) {
+        var elems = data.toString().split(' ');
+
+        cb(parseInt(elems[2]) + parseInt(elems[3]) + parseInt(elems[4]) + parseInt(elems[5]));
+    });
+};
+
+var getProcMem = function (pid, cb) {
+    fs.readFile('/proc/' + pid + '/status', function (err, data) {
+        var elems = data.toString().split('\n');
+        elems = elems[15].split('\t');
+        elems = elems[1].split(' ');
+        cb(elems[elems.length - 2]);
+    });
+};
+
+var monitor = function (pid, callback) {
+    getUserUsage(pid,function (startTimeU) {
+        getSysUsage(function (startTimeS) {
+            setTimeout(function () {
+                getUserUsage(pid, function (endTimeU) {
+                    getSysUsage(function (endTimeS) {
+                        getProcMem(pid, function (mem) {
+                            var cpu_user = endTimeU - startTimeU;
+                            var cpu_sys = endTimeS - startTimeS;
+                            var percentage = 100 * (cpu_user / cpu_sys);
+
+                            var result = {
+                                memory: mem,
+                                cpu: percentage
+                            };
+
+                            callback(result);
+
+                        });
+                    });
+                });
+            }, 1000);
+        });
+    });
+};
+
+exports.monitor = monitor;
