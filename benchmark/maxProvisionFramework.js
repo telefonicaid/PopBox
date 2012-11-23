0e12064e3ebd323ffa78c77865de2c1cb4b8f4a4
Aitor Magán <amagan@conwet.com>
2012-11-23 11:37:25 +0100

    Test adapted to the new version of Performance Framework

--- benchmark/maxProvisionFramework.js
+++ benchmark/maxProvisionFramework.js
@@ -1,3 +1,4 @@
+var dbPusher = require('./DBPusher.js');
 var rest = require('restler');
 var config = require('./config.js');
 var genProvision = require('./genProvision.js');
@@ -5,83 +6,105 @@ var async = require('async');
 var framework = require('performanceFramework');
 
 var testFrameWork = framework.describe('Max Provisions', 'This benchmark calculates the time elapsed to provision ' +
-    'am increasing number of queues.', ['Num Queues', 'Milliseconds'],['localhost'], '.');
+    'am increasing number of queues.', 'wijmo', ['Num Queues', 'Milliseconds'],['localhost'], '.');
 
 
 var doNtimes_queues = function (numQueues, payload_length, callback) {
 
-    var agentsTime = function () {
+    testFrameWork.test('Payload ' + payload_length + ' bytes', function(log, point) {
 
-        var functionArray = [];
-        var functionParallel;
 
-        for (var i = 0; i < config.agentsHosts.length; i++) {
-            var provision = genProvision.genProvision(numQueues, payload_length);
-            var host = config.agentsHosts[i].host;
-            var port = config.agentsHosts[i].port;
-            if (numQueues <= config.maxProvision.max_queues) {
-                functionParallel = function (host, port, numQueues) {
-                    return function functionDoNTimes(cb){
-                        _doNtimes_queues(provision, payload_length, host, port, numQueues, cb);
+        var agentsTime = function () {
+
+            var functionArray = [];
+            var functionParallel;
+
+            for (var i = 0; i < config.agentsHosts.length; i++) {
+                var provision = genProvision.genProvision(numQueues, payload_length);
+                var host = config.agentsHosts[i].host;
+                var port = config.agentsHosts[i].port;
+                if (numQueues <= config.maxProvision.max_queues) {
+                    functionParallel = function (host, port, numQueues) {
+                        return function functionDoNTimes(cb){
+                            _doNtimes_queues(provision, payload_length, host, port, numQueues, cb);
+                        }
+                    };
+                    functionArray.push(functionParallel(host,port, numQueues));
+                }
+                numQueues += config.maxProvision.queues_inteval;
+            }
+            async.parallel(functionArray, function () {
+                dbPusher.flushBBDD(function () {
+
+                    if (numQueues <= config.maxProvision.max_queues) {
+                        process.nextTick(agentsTime);
+                    } else {
+                        callback();
                     }
-                };
-                functionArray.push(functionParallel(host,port, numQueues));
-            }
-            numQueues += config.maxProvision.queues_inteval;
-        }
-        async.parallel(functionArray, function () {
-            if (numQueues <= config.maxProvision.max_queues) {
-                process.nextTick(agentsTime);
-            } else {
-                callback();
-            }
-        });
-    };
-    var _doNtimes_queues = function (provision, payload_length, host, port, numQueues, endCallback) {
-        'use strict';
+                });
+            });
+        };
 
-        var now, init, end, time, tps, message, nowToString, auxHost;
+        var _doNtimes_queues = function (provision, payload_length, host, port, numQueues, endCallback) {
+            'use strict';
 
-        init = new Date().valueOf();
-        rest.postJson(config.protocol + '://' + host + ':' + port + '/trans', provision).
-            on('complete', function (data, response) {
+            var now, init, end, time, tps, message, nowToString, auxHost;
 
-                if (response && response.statusCode === 200) {
+            init = new Date().valueOf();
+            rest.postJson(config.protocol + '://' + host + ':' + port + '/trans', provision).
+                on('complete', function (data, response) {
 
-                    end = new Date().valueOf();
-                    time = end - init;
+                    if (response && response.statusCode === 200) {
 
-                    now = new Date();
-                    tps = Math.round((numQueues / time) * 1000);
-                    message = numQueues + ' inboxes have been provisioned with ' +
-                        payload_length + ' bytes of payload in ' + time + ' ms with no errors (' + tps + ' tps)' ;
-                    nowToString = now.toTimeString().slice(0, 8);
-                    auxHost = (host === 'localhost') ? '127.0.0.1' : host;
+                        end = new Date().valueOf();
+                        time = end - init;
 
-                    var point = [numQueues, time];
+                        now = new Date();
+                        tps = Math.round((numQueues / time) * 1000);
+                        message = numQueues + ' inboxes have been provisioned with ' +
+                            payload_length + ' bytes of payload in ' + time + ' ms with no errors (' + tps + ' tps)' ;
+                        nowToString = now.toTimeString().slice(0, 8);
+                        auxHost = (host === 'localhost') ? '127.0.0.1' : host;
 
-                    testFrameWork.test(function(log, point) {
+                        console.log(message);
+
                         log(message);
                         point(numQueues, time);
-                    });
 
-                    endCallback();
-
-                } else {
-
-                    if ( data.errors ) {
                         endCallback();
+
+                    } else {
+
+                        if ( data.errors ) {
+                            endCallback();
+                        }
+
                     }
+                });
+        };
 
-                }
-            });
-    };
-
-    agentsTime();
+        agentsTime();
+    });
 };
 
-setTimeout(function() {
-    doNtimes_queues(config.maxProvision.start_number_provisions, config.payload_length, function () {
-        testFrameWork.done();
+var doNtimes = function (numQueues, payloadLength) {
+
+    doNtimes_queues(numQueues, payloadLength,function () {
+
+        //Increase the payload of the messages to be provisioned.
+        if (payloadLength < config.maxProvision.max_payload) {
+
+            payloadLength += config.maxProvision.payload_length_interval;
+            process.nextTick(function () {
+                doNtimes(numQueues, payloadLength);
+            });
+
+        } else {
+            testFrameWork.done();
+            dbPusher.closeDBConnections();
+        }
+
     });
-}, 3000);
+};
+
+doNtimes(config.maxProvision.start_number_provisions, config.payload_length);
