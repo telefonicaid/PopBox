0b8b690e0eeb9c5c78879acfed3b3d89da5397ff
Marcos Reyes <mru@tid.es>
2012-11-26 16:53:25 +0100

    improve error management in test

--- tests/test/inboxTest.js
+++ tests/test/inboxTest.js
@@ -11,7 +11,13 @@ var protocol = config.protocol;
 var trans, trans1 = {};
 
 describe('Inbox', function () {
+  "use strict";
 
+  function onError(err, response){
+    console.dir(err);
+    console.dir(response);
+    throw new Error({'err': err, 'response': response});
+  }
 
     after(function (done) {
         this.timeout(8000);
@@ -42,25 +48,25 @@ describe('Inbox', function () {
             function (callback) {
 
                 rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-                    trans).on('complete', function (data, response) {
+                    trans).on('success', function (data, response) {
                         callback();
-                    });
+                    }).on('error', onError);
 
             },
             function (callback) {
 
                 trans.payload='Test 2';
                 rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-                    trans).on('complete', function (data, response) {
+                    trans).on('success', function (data, response) {
                         callback();
-                    });
+                    }).on('error', onError);
 
             },
             function (callback) {
 
                 var checkQueue = function (queue, callback) {
                     rest.post(protocol + '://' + host + ':' + port + '/queue/' + queue + '/pop')
-                        .on('complete', function (data, response) {
+                        .on('success', function (data, response) {
                             data.should.not.have.property('error');
                             data.should.have.property('ok');
                             data.should.have.property('data');
@@ -68,7 +74,7 @@ describe('Inbox', function () {
                             data.data.should.include('Test');
                             data.data.should.include('Test 2');
                             callback();
-                        });
+                        }).on('error', onError);
                 };
 
                 checkQueue('q1', checkQueue.bind({}, 'q2', callback));
@@ -102,26 +108,26 @@ describe('Inbox', function () {
         async.series([
             function (callback) {
                 rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-                    transLow).on('complete', function (data, response) {
+                    transLow).on('success', function (data, response) {
                         callback();
-                    });
+                    }).on('error', onError);
             },
             function (callback) {
                 rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-                    transHigh).on('complete', function (data, response) {
+                    transHigh).on('success', function (data, response) {
                         callback();
-                    });
+                    }).on('error', onError);
             },
             function (callback) {
                 rest.post(protocol + '://' + host + ':' + port + '/queue/q1/pop?max=1')
-                    .on('complete', function (data, response) {
+                    .on('success', function (data, response) {
                     data.should.not.have.property('error');
                     data.should.have.property('ok');
                     data.should.have.property('data');
                     data.data.length.should.be.equal(1);
                         (data.data.pop()).should.be.equal('High priority');
                         callback();
-                    });
+                    }).on('error', onError);
             }
         ], function () {
             done();
@@ -145,22 +151,21 @@ describe('Inbox', function () {
             function (cb) {
                 rest.post(protocol + '://' + host + ':' + port + '/queue/q1/pop?timeout=1',
                     {headers: {'Accept': 'application/json'}})
-                    .on('complete', function (data, response) {
+                    .on('success', function (data, response) {
                         //console.log(data);
                        data.should.not.have.property('error');
                        data.should.have.property('ok');
                        data.should.have.property('data');
                        data.data.length.should.be.equal(0);
                         cb();
-
-                    });
+                    }).on('error', onError);
             },
             function (cb) {
                 setTimeout(function () {
                     rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-                        trans5).on('complete', function (data, response) {
+                        trans5).on('success', function (data, response) {
                             cb();
-                        });
+                        }).on('error', onError);
                 }, 2000);
 
             }
@@ -189,21 +194,21 @@ describe('Inbox', function () {
             function (cb) {
                 rest.post(protocol + '://' + host + ':' + port + '/queue/q1/pop?timeout=3',
                     {headers: {'Accept': 'application/json'}})
-                    .on('complete', function (data, response) {
+                    .on('success', function (data, response) {
                         data.should.not.have.property('error');
                         data.should.have.property('ok');
                         data.should.have.property('data');
                         data.data.length.should.be.equal(1);
                         data.data.should.include('Test timeout');
                         cb();
-                    });
+                    }).on('error', onError);
             },
             function (cb) {
                 setTimeout(function () {
                     rest.postJson(protocol + '://' + host + ':' + port + '/trans',
-                        trans6).on('complete', function (data, response) {
+                        trans6).on('success', function (data, response) {
                             cb();
-                        });
+                        }).on('error', onError);
                 }, 1000);
 
             }
