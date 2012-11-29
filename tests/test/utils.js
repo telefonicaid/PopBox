bace97abd21927130cd5b0d533a25912d5b879dd
Fernando Godino <fgodino@conwet.com>
2012-11-29 10:15:02 +0100

    refactoring reuests to avoid restler lib

---
+++ tests/test/utils.js
@@ -0,0 +1,31 @@
+http = require('http');
+
+var makeRequest = function (options, content, cb) {
+    'use strict';
+    var data = '';
+
+
+    var req = http.request(options, function (res) {
+
+        var o; //returned object from request
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function () {
+            cb(null, res, data);
+        });
+    });
+
+    req.on('error', function (e) {
+        cb(e, null, null);
+    });
+    if (options.method === 'POST' || options.method === 'PUT') {
+        req.write(content);
+    }
+
+    req.end();
+
+};
+
+exports.makeRequest = makeRequest;
