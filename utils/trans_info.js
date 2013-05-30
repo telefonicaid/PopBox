c484fa8d845f3197ecdb21149a39c4937bdcf956
Carlos Romero <brox@tid.es>
2013-05-30 17:57:52 +0200

    Add support for transaction info

---
+++ utils/trans_info.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var program = require('commander'),
+    request = require('request'),
+    util = require('util'),
+    i,
+    url,
+    trans;
+
+program
+    .version('0.0.1')
+    .option('-H, --host [hostname]', 'host, \'localhost\' by default', 'localhost')
+    .option('-P, --port [number]', 'port, 5001 by default', 5001, parseInt)
+    .option('-T, --trans [id]', 'transaction')
+    .option('-X, --secure', 'use HTTPS', false)
+    .parse(process.argv);
+url = util.format('%s://%s:%d/trans/%s', program.secure ? 'https' : 'http', program.host, program.port, program.trans);
+req_aux(url, function () {
+    req_aux(url + '/state', function(){});
+});
+function req_aux(url, done) {
+    console.log('url\n', url);
+    request.get({url: url, headers: {'Accept': 'application/json'}}, function (err, res, body) {
+        if (err) {
+            console.log('error\n', err);
+        }
+        else {
+            console.log('statusCode\n', res.statusCode);
+            console.log('headers\n', res.headers);
+            console.log('body\n', body);
+        }
+        done(err);
+    });
+}
+function range(val) {
+    return val.split('..').map(Number);
+}
+function list(val) {
+    return val.split(',');
+}
